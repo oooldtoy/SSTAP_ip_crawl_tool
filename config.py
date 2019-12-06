80a45077c30f50fc3d961fcffb83f754f644b835
oooldtoy <51742057+oooldtoy@users.noreply.github.com>
2019-12-06 17:03:31 +0800

    新增配置文件

---
+++ config.py
@@ -0,0 +1,10 @@
+version = 'v3.1'#版本号
+server = '127.0.0.1'#服务器地址
+web_server = '127.0.0.1'#网页服务器地址
+port = 5000 #服务器端口
+web_port = 80 #网页端口
+web_url = 'http://'+web_server+':'+str(web_port) #网页网址
+username = 'sstap'#管理员用户名
+password = '2137129481927'#管理员密码
+data_base = 'rules_data.db'#数据库路径
+crypto_key = '3789201928374612'#加密key，16位
