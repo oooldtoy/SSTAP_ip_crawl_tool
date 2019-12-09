74c5d450a56f7891d6aa63ad4254c25cd643e9ba
oooldtoy <51742057+oooldtoy@users.noreply.github.com>
2019-12-09 20:15:02 +0800

    增加进程名与版本号

--- sqlite3_mod.py
+++ sqlite3_mod.py
@@ -5,15 +5,15 @@ data_base = config.data_base
 def create_table():
     con = sqlite3.connect(data_base)
     cu = con.cursor()
-    cu.execute('create table rules (rules str not null,ip str not null,time int noy null)')
+    cu.execute('create table rules (rules str not null,process str not null,ip str not null,time int not null,version str not null)')
     con.commit()
     cu.close()
     con.close()
 
-def insert_data(rules,ip,time):
+def insert_data(rules,process,ip,time,version):
     con = sqlite3.connect(data_base)
     cu = con.cursor()
-    cu.execute("insert into rules(rules,ip,time) values(\"%s\",\"%s\",\"%d\")" %(rules,ip,time))
+    cu.execute("insert into rules(rules,process,ip,time,version) values(\"%s\",\"%s\",\"%s\",\"%d\",\"%s\")" %(rules,process,ip,time,version))
     con.commit()
     cu.close()
     con.close()
@@ -27,5 +27,5 @@ def delete_data(i):
     con.close()
 
 if __name__ == '__main__':
-    create_table()
+    #create_table()
     #insert_data('dsafas','fdsafas',23124.1234)
